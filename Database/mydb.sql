85da490cdc0bca3d7f68b563488dacceba685ce9
hkota69725 <46402752+hkota69725@users.noreply.github.com>
2019-03-28 12:25:00 -0500

    Update mydb.sql

--- Database/mydb.sql
+++ Database/mydb.sql
@@ -3,9 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2017 at 01:54 PM
--- Server version: 5.7.14
--- PHP Version: 7.0.10
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -15,16 +13,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `mydb`
---
 
--- --------------------------------------------------------
-
---
--- Table structure for table `message`
---
 
 CREATE TABLE `message` (
   `mid` int(11) NOT NULL,
